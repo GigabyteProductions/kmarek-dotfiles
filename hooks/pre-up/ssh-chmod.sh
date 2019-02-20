fdd49047ae17dce4023ea43bdc08a710e8c7357e
Kyle Marek <psppsn96@gmail.com>
2019-02-20 03:07:59 -0500

    Add ssh config
    hooks/pre-up/ssh-chmod.sh corrects the permissions on all the ssh/ files
    so ssh accepts the files (git only stores executable/non-executable bit)
    
    ssh/config includes ~/.ssh/config.d/*.conf
    
    profile.d/ssh-multiplex.sh accommodates ssh/config.d/90-multiplex.conf
    to create the referenced directory on login.
    
    local/bin/ssh_checkproxy facilitates the conditional ProxyJump in
    ssh/config.d/70-gigabyteproductions.net.conf based on a custom DNS
    provision on gigabyteproductions.net (ProxyJump is applied if TXT
    _ssh_proxy.gigabyteproductions.net is not "skip")

---
+++ hooks/pre-up/ssh-chmod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+# correct permissions on ssh configuration files before deployment
+
+bindir="${BASH_SOURCE[0]%/*}"
+bindir="${bindir:-/}"
+test -n "${bindir%%/*}" && bindir="$PWD/$bindir"
+
+basedir="$bindir/../.."
+
+chmod -cR go-rwx "$basedir/ssh"
