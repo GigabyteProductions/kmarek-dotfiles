d521ee23149b378a77a138d3a7e18f591be9ea38
Kyle Marek <psppsn96@gmail.com>
2019-06-25 22:15:19 -0400

    Add color aliases
    /etc/profile.d/ on Cygwin doesn't have the color aliases.

---
+++ tag-gnu/color-aliases-gnu.bash
@@ -0,0 +1,14 @@
+# color-aliases-gnu.bash
+
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias zgrep='zgrep --color=auto'
+alias zfgrep='zfgrep --color=auto'
+alias zegrep='zegrep --color=auto'
+alias xzgrep='xzgrep --color=auto'
+alias xzegrep='xzegrep --color=auto'
+alias xzfgrep='xzfgrep --color=auto'
+alias ll='ls -l --color=auto'
+alias l.='ls -d .* --color=auto'
+alias ls='ls --color=auto'
