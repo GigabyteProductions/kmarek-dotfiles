8ed4ac0b3135b6cdf8016659ebce6c9d5f71cede
Kyle Marek <psppsn96@gmail.com>
2020-05-22 19:36:47 -0400

    stop clearing my fucking terminal on logout...

---
+++ tag-cygwin/bashrc.d/cygwin-no-clear-screen.bash
@@ -0,0 +1,8 @@
+# cygwin-no-clear-screen.bash
+
+# to combat these lines from Cygwin's /etc/bash.bash_logout:
+# # when leaving the console clear the screen to increase privacy
+# if [ "$SHLVL" = 1 ]; then
+#   [ -x /usr/bin/clear ] && /usr/bin/clear
+# fi
+SHLVL=0
