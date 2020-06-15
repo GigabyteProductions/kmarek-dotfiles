56ffdb926b5a8fe8569c141b8e20bd9b0cea58a1
Kyle Marek <psppsn96@gmail.com>
2020-06-14 23:10:22 -0400

    prompt_ssh: use ssh color if process tree has sshd

--- bashrc.d/05prompt_ssh.bash
+++ bashrc.d/05prompt_ssh.bash
@@ -1,7 +1,7 @@
 # prompt_ssh.bash
 
 # If in an ssh session, use bright magenta
-if [ -n "$SSH_CONNECTION" ]; then
+if [ -n "$SSH_CONNECTION" -o -n "$pstree_has_sshd" ]; then
 	PS1_UCOLOR="95"
 	PS1_HCOLOR="95"
 fi
