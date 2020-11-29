f351db1147854949e4138f49f37ed4d685541f83
Kyle Marek <psppsn96@gmail.com>
2020-11-28 23:13:05 -0500

    Improve escape.bash for legible escaping
    For example, if something can be quoted, simply quote it instead of
    outputting printf\ \'%s\'\ nonsense

--- bashrc.d/escape.bash
+++ bashrc.d/escape.bash
@@ -1,5 +1,7 @@
 # escape.bash
 
+# TODO: handle LANG=C
+
 escape()
 {
 	local str=''
@@ -9,7 +11,37 @@ escape()
 
 	for arg in "$@"; do
 		local e
-		printf -v e '%s%q' "$prefix" "$arg"
+		printf -v e '%q' "$arg"
+
+		# if LANG=C but extended ASCII is present
+		if [ "$LANG" = "C" ] && [[ "$arg" =~ [^\xA8-\xFE] ]]; then
+			e="${prefix}${arg@Q}"
+
+		# doesn't need escaping
+		elif [ "$arg" = "$e" ]; then
+			printf -v e '%s%s' "$prefix" "$arg"
+
+		# can be escaped with double-quotes
+		# "str a b c"
+		elif ! [[ "$arg" =~ [[:cntrl:]\$\`\"\\\!] ]]; then
+			printf -v e '%s"%s"' "$prefix" "$arg"
+
+		# can be escaped with single-quotes
+		# 'str a "b" c'
+		elif ! [[ "$arg" =~ [[:cntrl:]\'] ]]; then
+			printf -v e "%s'%s'" "$prefix" "$arg"
+
+		# can be escaped with double-quotes, escaping other double-quotes
+		# "str 'a' \"b\" c"
+		elif ! [[ "$arg" =~ [[:cntrl:]\$\`\\\!] ]]; then
+			printf -v e '%s"%s"' "$prefix" "${arg//\"/\\\"}"
+
+		# can be escaped with single-quotes, escaping other single-quotes
+		# 'str '\''a'\'' "b" c'
+		elif ! [[ "$arg" =~ [[:cntrl:]] ]]; then
+			printf -v e "%s\'%s\'" "$prefix" "${arg//\'/\'\\\'\'}"
+
+		fi
 		prefix=' '
 		str+="$e"
 	done
