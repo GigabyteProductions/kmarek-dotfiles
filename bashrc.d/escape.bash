75229ae9ee43b67e0ad9916ac2bc1a5e78a0df1b
Kyle Marek <psppsn96@gmail.com>
2021-05-18 03:53:03 -0400

    Update escape.bash
    - Disable LANG=C logic
    - apply $prefix at end

--- bashrc.d/escape.bash
+++ bashrc.d/escape.bash
@@ -2,6 +2,7 @@
 
 # TODO: handle LANG=C
 
+# TODO: printf to variable
 escape()
 {
 	local str=''
@@ -13,37 +14,48 @@ escape()
 		local e
 		printf -v e '%q' "$arg"
 
-		# if LANG=C but extended ASCII is present
-		if [ "$LANG" = "C" ] && [[ "$arg" =~ [^\xA8-\xFE] ]]; then
-			e="${prefix}${arg@Q}"
+		## if LANG=C but extended ASCII is present
+		#if [ "$LANG" = "C" ] && [[ "$arg" =~ [^\xA8-\xFE] ]]; then
+		#	# Note: as of writing (bash 5.0.17), this put single quotes around
+		#	# numeric arguments.
+		#	printf -v e '%s' "${arg@Q}"
 
 		# doesn't need escaping
 		elif [ "$arg" = "$e" ]; then
-			printf -v e '%s%s' "$prefix" "$arg"
+			#printf -v e '%s' "$arg"
+			true
 
 		# can be escaped with double-quotes
 		# "str a b c"
 		elif ! [[ "$arg" =~ [[:cntrl:]\$\`\"\\\!] ]]; then
-			printf -v e '%s"%s"' "$prefix" "$arg"
+			printf -v e '"%s"' "$arg"
 
 		# can be escaped with single-quotes
 		# 'str a "b" c'
 		elif ! [[ "$arg" =~ [[:cntrl:]\'] ]]; then
-			printf -v e "%s'%s'" "$prefix" "$arg"
+			printf -v e "'%s'" "$arg"
 
 		# can be escaped with double-quotes, escaping other double-quotes
 		# "str 'a' \"b\" c"
 		elif ! [[ "$arg" =~ [[:cntrl:]\$\`\\\!] ]]; then
-			printf -v e '%s"%s"' "$prefix" "${arg//\"/\\\"}"
+			printf -v e '"%s"' "${arg//\"/\\\"}"
 
 		# can be escaped with single-quotes, escaping other single-quotes
 		# 'str '\''a'\'' "b" c'
 		elif ! [[ "$arg" =~ [[:cntrl:]] ]]; then
-			printf -v e "%s\'%s\'" "$prefix" "${arg//\'/\'\\\'\'}"
+			printf -v e "\'%s\'" "${arg//\'/\'\\\'\'}"
+
+		# control character, use bash's %q escaping
+		else
+			#printf -v e '%s' "$e"
+			true
 
 		fi
-		prefix=' '
+
+		# add prefix and append
+		printf -v e '%s%s' "$prefix" "$e"
 		str+="$e"
+		prefix=' '
 	done
 
 	echo "$str"
