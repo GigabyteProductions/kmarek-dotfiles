9f7e00fa38b4bf893d73ecbcc4517355d4f6b425
Kyle Marek <psppsn96@gmail.com>
2022-06-13 19:16:15 -0400

    Update vim editor.bash
    - Use the first to exist: vimx,vim,vi
    - Alias the ones on the right to the first found

--- tag-editor-vim/bashrc.d/editor.bash
+++ tag-editor-vim/bashrc.d/editor.bash
@@ -1,3 +1,26 @@
 # editor.bash
 
+# remove any existing command "overrides"
+# so bash will only find executables in $PATH
+unalias vi >/dev/null 2>&1
+unset vi
+unalias vim >/dev/null 2>&1
+unset vim
+unalias vimx >/dev/null 2>&1
+unset vimx
+
+# default to vi, which should be on every modern unix system OOTB
+export EDITOR=vi
+
+# use vim if it exists
+if command -v vim >/dev/null 2>&1; then
 export EDITOR=vim
+alias vi=vim
+fi
+
+# use vimx if it exists
+if command -v vimx >/dev/null 2>&1; then
+export EDITOR=vimx
+alias vim=vimx
+alias vi=vim
+fi
