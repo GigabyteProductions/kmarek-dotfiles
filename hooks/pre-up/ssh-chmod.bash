22028e02cbdaec08bedfaf95516ce9b6fe368387
Kyle Marek <psppsn96@gmail.com>
2022-12-18 16:25:04 -0500

    Add basedir path comment

--- hooks/pre-up/ssh-chmod.bash
+++ hooks/pre-up/ssh-chmod.bash
@@ -13,6 +13,8 @@ bindir="${BASH_SOURCE[0]%/*}"
 bindir="${bindir:-/}"
 test -n "${bindir%%/*}" && bindir="$PWD/$bindir"
 
+# find the root of this dotfiles repository relative to this hook
+# (this hook is in hooks/pre-up/)
 basedir="$bindir/../.."
 
 chmod -cR go-rwx "$basedir/ssh" "$basedir"/tag-*/ssh
