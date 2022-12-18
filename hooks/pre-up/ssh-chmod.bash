7c544eb8a896240071bc9a73186cdb9e7a6a8262
Kyle Marek <psppsn96@gmail.com>
2022-12-18 16:22:40 -0500

    Adjust directory pre-create logic

--- hooks/pre-up/ssh-chmod.bash
+++ hooks/pre-up/ssh-chmod.bash
@@ -4,14 +4,36 @@
 
 set -e
 
-# create ~/.ssh before rcup would with incorrect permissions
-test -d ~/.ssh || mkdir -v --mode 0700 ~/.ssh
-
 # correct permissions on ssh configuration files before deployment
 
 bindir="${BASH_SOURCE[0]%/*}"
 bindir="${bindir:-/}"
 test -n "${bindir%%/*}" && bindir="$PWD/$bindir"
+
+# Add g=w,o=w to the current umask (removing these bits from new files)
+umask="$(umask)"
+umask="$(printf '%03o' "$((8#$umask | 8#022))")"
+umask "$umask";
+
+# precreate_candidates are the directories that may need to be created
+precreate_candidates=(
+	~/.ssh
+	~/.ssh/config.d
+)
+
+# precreate are the directories that do not exist yet
+precreate=()
+for dir in "${precreate_candidates[@]}"; do
+	if [ ! -d "$dir" ]; then
+		precreate+=("$dir")
+	fi
+done
+
+# pre-create directories that require restrictive permissions
+# (umask restricts the mode, avoiding an extra call to chmod)
+if [ "${#precreate[@]}" -gt 0 ]; then
+	mkdir -v -- "${precreate[@]}"
+fi
 
 # find the root of this dotfiles repository relative to this hook
 # (this hook is in hooks/pre-up/)
